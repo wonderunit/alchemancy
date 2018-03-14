943c4a1488791922369a442acab2bd3940a3d24b
Eric Skogen <audionerd@users.noreply.github.com>
2018-03-13 22:16:28 -0500

    BrushNodeFilter extends PIXI.Filter

--- src/js/sketch-pane/brush/brush-node-filter.js
+++ src/js/sketch-pane/brush/brush-node-filter.js
@@ -1,46 +1,60 @@
 /* global PIXI */
-module.exports = class BrushNodeFilter {
-  constructor (fragPath) {
-    this.fragPath = fragPath
+
+const fragment = require('./brushnode.frag')
+
+module.exports = class BrushNodeFilter extends PIXI.Filter {
+  constructor () {
+    super(
+      null,
+      fragment,
+      {
+        uRed: { type: '1f', value: 0.5 },
+        uGreen: { type: '1f', value: 0.5 },
+        uBlue: { type: '1f', value: 0.5 },
+        uOpacity: { type: '1f', value: 1 },
+        uRotation: { type: '1f', value: 0 },
+        uBleed: { type: '1f', value: 0 },
+        uGrainRotation: { type: '1f', value: 0 },
+        uGrainScale: { type: '1f', value: 1 },
+        u_size: { type: '1f', value: 100 },
+        u_texture_size: { type: '1f', value: 100 },
+        u_x_offset: { type: '1f', value: 0 },
+        u_y_offset: { type: '1f', value: 0 },
+        u_grain_zoom: { type: '1f', value: 1 },
+        u_brushTex: { type: 'sampler2D', value: '' },
+        u_grainTex: { type: 'sampler2D', value: '' },
+
+        u_offset_px: { type: 'vec2' },
+
+        dimensions: { type: 'vec2', value: [0.0, 0.0] }
+      }
+    )
+
+    this.padding = 0
+    this.blendMode = PIXI.BLEND_MODES.NORMAL
+
+    // via http://www.html5gamedevs.com/topic/29327-guide-to-pixi-v4-filters/
+    this.autoFit = false
   }
-  async load () {
-    return new Promise((resolve, reject) => {
-      const loader = new PIXI.loaders.Loader()
-      loader.add('brushnode.frag', this.fragPath)
-      loader.load((loader, resources) => {
-        if (resources['brushnode.frag'].error) {
-          reject(resources['brushnode.frag'].error)
-        }
-        let uniforms = {
-          uRed: { type: '1f', value: 0.5 },
-          uGreen: { type: '1f', value: 0.5 },
-          uBlue: { type: '1f', value: 0.5 },
-          uOpacity: { type: '1f', value: 1 },
-          uRotation: { type: '1f', value: 0 },
-          uBleed: { type: '1f', value: 0 },
-          uGrainRotation: { type: '1f', value: 0 },
-          uGrainScale: { type: '1f', value: 1 },
-          u_size: { type: '1f', value: 100 },
-          u_texture_size: { type: '1f', value: 100 },
-          u_x_offset: { type: '1f', value: 0 },
-          u_y_offset: { type: '1f', value: 0 },
-          u_grain_zoom: { type: '1f', value: 1 },
-          u_brushTex: { type: 'sampler2D', value: '' },
-          u_grainTex: { type: 'sampler2D', value: '' },
 
-          u_offset_px: { type: 'vec2', value: [0, 0] },
+  // TODO move this to sketch-pane.js?
+  // via http://www.html5gamedevs.com/topic/29327-guide-to-pixi-v4-filters/
+  apply (filterManager, input, output, clear) {
+    this.uniforms.dimensions[0] = input.sourceFrame.width
+    this.uniforms.dimensions[1] = input.sourceFrame.height
 
-          dimensions: { type: 'vec2', value: [0, 0] }
-        }
-        this.shader = new PIXI.Filter(null, resources['brushnode.frag'].data, uniforms)
-        this.shader.padding = 0
-        this.shader.blendMode = PIXI.BLEND_MODES.NORMAL
+    // TODO use this
+    // this.uniforms.filterMatrix = filterManager.calculateSpriteMatrix(
+    //   new PIXI.Matrix(),
+    //   this.brushImageSprites[brush.settings.brushImage]
+    // )
 
-        // via http://www.html5gamedevs.com/topic/29327-guide-to-pixi-v4-filters/
-        this.shader.autoFit = false
+    // TODO use this
+    // this.uniforms.grainFilterMatrix = filterManager.calculateSpriteMatrix(
+    //   new PIXI.Matrix(),
+    //   this.grainImageSprites[brush.settings.grainImage]
+    // )
 
-        resolve(resources['brushnode.frag'])
-      })
-    })
+    filterManager.applyFilter(this, input, output, clear)
   }
 }
