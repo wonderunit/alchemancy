c2685486c3fe48220999583342d5050ebeac753b
Eric Skogen <audionerd@users.noreply.github.com>
2018-03-29 16:58:14 -0500

    prettier format brush and brushes

--- src/js/sketch-pane/brush/brush.js
+++ src/js/sketch-pane/brush/brush.js
@@ -1,8 +1,7 @@
 const defaultBrushSettings = {
-  
   // GENERAL
-  name: "default", // Name of the brush preset
-  blendMode: "normal", // Blend mode of the stroke (not node) on the layer
+  name: 'default', // Name of the brush preset
+  blendMode: 'normal', // Blend mode of the stroke (not node) on the layer
   sizeLimitMax: 1, // UI limit for size
   sizeLimitMin: 0,
   opacityMax: 1, // UI limit for opacity
@@ -12,10 +11,10 @@ const defaultBrushSettings = {
   spacing: 0, // spacing in between brush nodes
 
   // TEXTURES
-  brushImage: "brushcharcoal", // Name alias of brush alpha
+  brushImage: 'brushcharcoal', // Name alias of brush alpha
   brushRotation: 0, // rotation of texture (0,90,180,270)
   brushImageInvert: false, // invert texture
-  grainImage: "graingrid", // Name alias of brush grain texture
+  grainImage: 'graingrid', // Name alias of brush grain texture
   grainRotation: 0,
   grainImageInvert: false,
 
@@ -27,23 +26,20 @@ const defaultBrushSettings = {
   randomOffset: true, // on strokeDown, choose a random grain offset
 
   // STYLUS
-  azimuth: true, 
+  azimuth: true,
   pressureOpacity: 1, // % Pressure affects opacity
   pressureSize: 1, // % Pressure affects size
-  pressureBleed: 0, // 
-  tiltAngle: 0, // % the title angle affects the below params 
+  pressureBleed: 0, //
+  tiltAngle: 0, // % the title angle affects the below params
   tiltOpacity: 1, // % opacity altered by the tilt
   tiltGradiation: 0, // % opacity is gradiated by the tilt
   tiltSize: 1, // % size altered by the tilt
 
-  orientToScreen: true, // orient the brush shape to the rotation of the screen
-
+  orientToScreen: true // orient the brush shape to the rotation of the screen
 }
 
 module.exports = class Brush {
-
   constructor (settings) {
     this.settings = Object.assign({}, defaultBrushSettings, settings)
   }
-
 }
--- src/js/sketch-pane/brush/brushes.js
+++ src/js/sketch-pane/brush/brushes.js
@@ -1,146 +1,143 @@
+const PIXI = require('pixi.js')
+
 const Brush = require('./brush.js')
 const loader = new PIXI.loaders.Loader()
 
-
 const standardBrushes = [
-
   {
-    name: "default", // Name of the brush preset
-    descriptiveName: "Default Brush", // Name of the brush preset
+    name: 'default', // Name of the brush preset
+    descriptiveName: 'Default Brush' // Name of the brush preset
   },
 
   {
-    name: "pencil", // Name of the brush preset
-    descriptiveName: "Pencil", // Name of the brush preset
-    brushImage: "brushmediumoval", // Name alias of brush alpha
-    grainImage: "grainpaper4", // Name alias of brush grain texture
+    name: 'pencil', // Name of the brush preset
+    descriptiveName: 'Pencil', // Name of the brush preset
+    brushImage: 'brushmediumoval', // Name alias of brush alpha
+    grainImage: 'grainpaper4', // Name alias of brush grain texture
     pressureOpacity: 0.7, // % Pressure affects opacity
     pressureSize: 0.8, // % Pressure affects opacity
-    scale: .8, // Scale of the grain texture. 0 super tiny, 100 super large
-    tiltOpacity: .3, // % opacity altered by the tilt
+    scale: 0.8, // Scale of the grain texture. 0 super tiny, 100 super large
+    tiltOpacity: 0.3, // % opacity altered by the tilt
     tiltSize: 1, // % size altered by the tilt
     movement: 1,
     pressureBleed: 1,
-    spacing: 0.05, // spacing in between brush nodes
+    spacing: 0.05 // spacing in between brush nodes
   },
 
   {
-    name: "brushpen", // Name of the brush preset
-    descriptiveName: "Brush Pen Bobby", // Name of the brush preset
-    brushImage: "teardrop", // Name alias of brush alpha
-    grainImage: "hardwood", // Name alias of brush grain texture
+    name: 'brushpen', // Name of the brush preset
+    descriptiveName: 'Brush Pen Bobby', // Name of the brush preset
+    brushImage: 'teardrop', // Name alias of brush alpha
+    grainImage: 'hardwood', // Name alias of brush grain texture
     pressureOpacity: 0.3, // % Pressure affects opacity
     scale: 0.5, // Scale of the grain texture. 0 super tiny, 100 super large
     movement: 0.7,
-    sizecale: 0.6, // Scale of the grain texture. 0 super tiny, 100 super large
+    sizecale: 0.6 // Scale of the grain texture. 0 super tiny, 100 super large
   },
-  
+
   {
-    name: "pen", // Name of the brush preset
-    descriptiveName: "Pen", // Name of the brush preset
-    brushImage: "brushhard", // Name alias of brush alpha
-    grainImage: "grainpaper2", // Name alias of brush grain texture
+    name: 'pen', // Name of the brush preset
+    descriptiveName: 'Pen', // Name of the brush preset
+    brushImage: 'brushhard', // Name alias of brush alpha
+    grainImage: 'grainpaper2', // Name alias of brush grain texture
     pressureOpacity: 0.5, // % Pressure affects opacity
-    pressureSize: .8, // % Pressure affects size
+    pressureSize: 0.8, // % Pressure affects size
     sizecale: 0.8, // Scale of the grain texture. 0 super tiny, 100 super large
     pressureBleed: 2,
     tiltSize: 3.8,
     tiltOpacity: 1,
     movement: 0.9,
-    spacing: 0.05, // spacing in between brush nodes
-
+    spacing: 0.05 // spacing in between brush nodes
   },
 
   {
-    name: "copic", // Name of the brush preset
-    descriptiveName: "Copic", // Name of the brush preset
-    brushImage: "brushmediumovalhallow", // Name alias of brush alpha
-    grainImage: "grainpaper2", // Name alias of brush grain texture
+    name: 'copic', // Name of the brush preset
+    descriptiveName: 'Copic', // Name of the brush preset
+    brushImage: 'brushmediumovalhallow', // Name alias of brush alpha
+    grainImage: 'grainpaper2', // Name alias of brush grain texture
     pressureOpacity: 0.2, // % Pressure affects opacity
-    pressureSize: .9, // % Pressure affects size
+    pressureSize: 0.9, // % Pressure affects size
     tiltSize: 1,
     tiltOpacity: 1,
-    movement: 0.5,
+    movement: 0.5
   },
 
   {
-    name: "charcoal", // Name of the brush preset
-    descriptiveName: "Charcoal", // Name of the brush preset
-    brushImage: "brushcharcoal", // Name alias of brush alpha
-    grainImage: "graincanvas", // Name alias of brush grain texture
+    name: 'charcoal', // Name of the brush preset
+    descriptiveName: 'Charcoal', // Name of the brush preset
+    brushImage: 'brushcharcoal', // Name alias of brush alpha
+    grainImage: 'graincanvas', // Name alias of brush grain texture
     pressureOpacity: 0.4, // % Pressure affects opacity
-    pressureSize: .8, // % Pressure affects size
+    pressureSize: 0.8, // % Pressure affects size
     sizecale: 1, // Scale of the grain texture. 0 super tiny, 100 super large
     tiltOpacity: 0.4, // % opacity altered by the tilt
     tiltSize: 1, // % size altered by the tilt
     spacing: 0.05, // spacing in between brush nodes
-    pressureBleed: 0.5,
+    pressureBleed: 0.5
   },
 
   {
-    name: "watercolor", // Name of the brush preset
-    descriptiveName: "Watercolor", // Name of the brush preset
-    brushImage: "brushwatercolor", // Name alias of brush alpha
-    grainImage: "grainwatercolor1", // Name alias of brush grain texture
+    name: 'watercolor', // Name of the brush preset
+    descriptiveName: 'Watercolor', // Name of the brush preset
+    brushImage: 'brushwatercolor', // Name alias of brush alpha
+    grainImage: 'grainwatercolor1', // Name alias of brush grain texture
     pressureOpacity: 1, // % Pressure affects opacity
     pressureSize: 1, // % Pressure affects size
     sizecale: 1, // Scale of the grain texture. 0 super tiny, 100 super large
     tiltOpacity: 1, // % opacity altered by the tilt
     tiltSize: 1, // % size altered by the tilt
     spacing: 0.05, // spacing in between brush nodes
-    pressureBleed: 0.5,
+    pressureBleed: 0.5
   },
 
   {
-    name: "clouds", // Name of the brush preset
-    descriptiveName: "Clouds", // Name of the brush preset
-    brushImage: "brushclouds", // Name alias of brush alpha
-    grainImage: "grainclouds", // Name alias of brush grain texture
+    name: 'clouds', // Name of the brush preset
+    descriptiveName: 'Clouds', // Name of the brush preset
+    brushImage: 'brushclouds', // Name alias of brush alpha
+    grainImage: 'grainclouds', // Name alias of brush grain texture
     pressureOpacity: 1, // % Pressure affects opacity
     pressureSize: 1, // % Pressure affects size
     sizecale: 1, // Scale of the grain texture. 0 super tiny, 100 super large
     tiltOpacity: 1, // % opacity altered by the tilt
     tiltSize: 1, // % size altered by the tilt
     spacing: 0.1, // spacing in between brush nodes
-    movement: 1, // % the grain is offset as the brush moves. 0 static. 100 rolling. 100 is like paper
+    movement: 1 // % the grain is offset as the brush moves. 0 static. 100 rolling. 100 is like paper
   },
 
   {
-    name: "slate", // Name of the brush preset
-    descriptiveName: "Clouds", // Name of the brush preset
-    brushImage: "flatbrush", // Name alias of brush alpha
-    grainImage: "grainslate", // Name alias of brush grain texture
+    name: 'slate', // Name of the brush preset
+    descriptiveName: 'Clouds', // Name of the brush preset
+    brushImage: 'flatbrush', // Name alias of brush alpha
+    grainImage: 'grainslate', // Name alias of brush grain texture
     pressureOpacity: 1, // % Pressure affects opacity
     pressureSize: 1, // % Pressure affects size
     sizecale: 1, // Scale of the grain texture. 0 super tiny, 100 super large
     tiltOpacity: 1, // % opacity altered by the tilt
     tiltSize: 1, // % size altered by the tilt
     movement: 1, // % the grain is offset as the brush moves. 0 static. 100 rolling. 100 is like paper
-    spacing: 0.05, // spacing in between brush nodes
-
-  },
-
+    spacing: 0.05 // spacing in between brush nodes
+  }
 ]
 
-
 let brushes = {}
-//let brushResources = new BrushResources()
 
-standardBrushes.forEach((brush)=> {
+standardBrushes.forEach(brush => {
   brushes[brush.name] = new Brush(brush)
- // brushResources.load(brushes[brush.name])
 
-  if (!loader.resources[brushes[brush.name].settings.brushImage]){
-    loader.add(brushes[brush.name].settings.brushImage, 'src/img/brush/' + brushes[brush.name].settings.brushImage + '.png')
+  if (!loader.resources[brushes[brush.name].settings.brushImage]) {
+    loader.add(
+      brushes[brush.name].settings.brushImage,
+      'src/img/brush/' + brushes[brush.name].settings.brushImage + '.png'
+    )
   }
-  if (!loader.resources[brushes[brush.name].settings.grainImage]){
-    loader.add(brushes[brush.name].settings.grainImage, 'src/img/brush/' + brushes[brush.name].settings.grainImage + '.png')
+  if (!loader.resources[brushes[brush.name].settings.grainImage]) {
+    loader.add(
+      brushes[brush.name].settings.grainImage,
+      'src/img/brush/' + brushes[brush.name].settings.grainImage + '.png'
+    )
   }
 })
 
-// loader.load((loader, resources) => {
-// })
-
-let value = {brushes: brushes, brushResources: loader}
+let value = { brushes: brushes, brushResources: loader }
 
 module.exports = value
