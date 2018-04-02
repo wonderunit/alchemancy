aab498d69fadd00619882565e95c75225510dd32
Eric Skogen <audionerd@users.noreply.github.com>
2018-04-02 15:10:09 -0500

    compile browser and commonjs versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,50 @@
 const path = require('path')
 
-module.exports = {
-  mode: process.env.MODE,
-  entry: {
-    'sketch-pane': './src/js/index.js'
-  },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendor',
-          chunks: 'all'
+const createConfig = opt => {
+  return {
+    mode: process.env.MODE,
+    entry: {
+      'sketch-pane': './src/js/index.js'
+    },
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendor',
+            chunks: 'all'
+          }
         }
       }
-    }
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-    library: 'SketchPane',
-    libraryTarget: 'umd',
-    publicPath: '/dist'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(glsl|frag|vert)$/,
-        loader: 'shader-loader'
-      }
-    ]
-  },
-  ...process.env.MODE === 'development'
-    ? {
-      serve: {
-        dev: {
-          publicPath: '/dist'
+    },
+    output: {
+      filename: opt.output.filename,
+      path: path.resolve(__dirname, 'dist'),
+      library: 'SketchPane',
+      libraryTarget: opt.output.libraryTarget,
+      publicPath: '/dist'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(glsl|frag|vert)$/,
+          loader: 'shader-loader'
+        }
+      ]
+    },
+    ...process.env.WEBPACK_SERVE === 'development'
+      ? {
+        serve: {
+          dev: {
+            publicPath: '/dist'
+          }
         }
       }
-    }
-    : {}
+      : {}
+  }
 }
+
+module.exports = [
+  createConfig({ output: { filename: '[name].browser.js', libraryTarget: 'var' } }),
+  createConfig({ output: { filename: '[name].common.js', libraryTarget: 'commonjs2' } })
+]
