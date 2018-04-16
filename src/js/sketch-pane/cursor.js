2c5c805940552137690a5fae09badb4033b472ca
Eric Skogen <audionerd@users.noreply.github.com>
2018-04-16 17:27:38 -0500

    extract Cursor sprite to its own class

---
+++ src/js/sketch-pane/cursor.js
@@ -0,0 +1,42 @@
+const PIXI = require('pixi.js')
+
+module.exports = class Cursor extends PIXI.Sprite {
+  constructor (container) {
+    super()
+    this.container = container
+
+    this.name = 'cursorSprite'
+
+    this.gfx = new PIXI.Graphics()
+    this.addChild(this.gfx)
+
+    this.updateSize()
+  }
+  render (e) {
+    let point = this.container.localizePoint(e)
+    this.position.set(point.x, point.y)
+    this.anchor.set(0.5)
+    this.container.app.view.style.cursor = 'none'
+  }
+  updateSize () {
+    let resolution = 1
+    let size = this.container.brushSize * 0.7 // optical, approx.
+
+    this.gfx
+      .clear()
+      // increase bounds (hack to to avoid clipping)
+      .lineStyle(resolution, 0xffffff, 0)
+      .drawCircle(0, 0, Math.ceil(size * resolution) + (resolution * 2))
+      .closePath()
+      // increase bounds (smaller white circle)
+      .lineStyle(resolution, 0xffffff)
+      .drawCircle(0, 0, Math.ceil(size * resolution) - resolution)
+      .closePath()
+      // increase bounds (actual size black circle)
+      .lineStyle(resolution, 0x000000)
+      .drawCircle(0, 0, Math.ceil(size * resolution))
+      .closePath()
+
+    this.texture = this.gfx.generateCanvasTexture()
+  }
+}
--- src/js/sketch-pane/sketch-pane.js
+++ src/js/sketch-pane/sketch-pane.js
@@ -4,49 +4,9 @@ const paper = require('paper')
 const Util = require('./util')
 const brushes = require('./brush/brushes')
 const BrushNodeFilter = require('./brush/brush-node-filter')
+const Cursor = require('./cursor')
 
 const LayersCollection = require('./layers-collection')
-
-class Cursor extends PIXI.Sprite {
-  constructor (container) {
-    super()
-    this.container = container
-
-    this.name = 'cursorSprite'
-
-    this.gfx = new PIXI.Graphics()
-    this.addChild(this.gfx)
-
-    this.updateSize()
-  }
-  render (e) {
-    let point = this.container.localizePoint(e)
-    this.position.set(point.x, point.y)
-    this.anchor.set(0.5)
-    this.container.app.view.style.cursor = 'none'
-  }
-  updateSize () {
-    let resolution = 1
-    let size = this.container.brushSize * 0.7 // optical, approx.
-
-    this.gfx
-      .clear()
-      // increase bounds (hack to to avoid clipping)
-      .lineStyle(resolution, 0xffffff, 0)
-      .drawCircle(0, 0, Math.ceil(size * resolution) + (resolution * 2))
-      .closePath()
-      // increase bounds (smaller white circle)
-      .lineStyle(resolution, 0xffffff)
-      .drawCircle(0, 0, Math.ceil(size * resolution) - resolution)
-      .closePath()
-      // increase bounds (actual size black circle)
-      .lineStyle(resolution, 0x000000)
-      .drawCircle(0, 0, Math.ceil(size * resolution))
-      .closePath()
-
-    this.texture = this.gfx.generateCanvasTexture()
-  }
-}
 
 module.exports = class SketchPane {
   constructor (options = { backgroundColor: 0xffffff }) {
